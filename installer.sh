7eb45f0a7345c123f97c2911cd90a102c269ed8a
smarthome707 <65874391+smarthome707@users.noreply.github.com>
2020-05-24 23:49:12 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,24 +1,4 @@
 #!/usr/bin/env bash
-set -e
-
-function error { echo -e "[Error] $*"; exit 1; }
-function warn  { echo -e "[Warning] $*"; }
-
-warn "This installer is no longer supported."
-warn ""
-warn "Home Assistant might work today, tomorrow maybe not."
-warn ""
-warn "If you want more control over your own system, run"
-warn "Home Assistant as a VM or run Home Assistant Core"
-warn "via a Docker container."
-warn ""
-echo 'Please typ "not supported" to continue this installation'
-read "not supported"
-if [ "$not supported" != "not supported" ]
-then
-  echo "OK, bye!"
-  exit 1
-fi
 
 ARCH=$(uname -m)
 DOCKER_BINARY=/usr/bin/docker
